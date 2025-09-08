c0edbdb6d52f3626b2876e54a8063c40e5616441
uccs <1500846601@qq.com>
2025-09-08 11:17:52 +0800

    init template

---
+++ apps/app1/vite.config.ts
@@ -0,0 +1,29 @@
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import { resolve } from "path";
+import { defineConfig, loadEnv } from "vite";
+import checker from "vite-plugin-checker";
+
+// https://vite.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    base: env.VITE_BASE_URL,
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "src"),
+      },
+    },
+    plugins: [
+      vue(),
+      vueJsx({
+        transformOn: true,
+        mergeProps: true,
+      }),
+      checker({
+        vueTsc: true,
+        typescript: true,
+      }),
+    ],
+  };
+});
